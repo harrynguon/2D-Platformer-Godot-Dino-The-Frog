f1cf83c58a6d846c220d70d5710733edc8c0d0f5
Harry Nguon <harrynguon@gmail.com>
2020-05-12 23:09:11 +1200

    Added third level with water, added end level, todo: music, sound effects, victory menu, main menu

--- Levels/Level_1.gd
+++ Levels/Level_1.gd
@@ -1,10 +1,54 @@
+# Signal handler.
+
 extends Node2D
 
-func _on_SecondRoomDetector_body_entered(body):
-	$Camera2D.current = false
-	$Camera2D2.current = true
+onready var invisWall = preload("res://InvisWall.tscn")
+
+var currentRoom = 0
+var activated_wall = false
+	
+func tweenIt(start : Vector2, end : Vector2) -> Tween:
+	var tween = $Tween
+	tween.interpolate_property($Camera2D,\
+		"position",\
+	 	start, \
+		end,\
+		0.3,\
+		Tween.TRANS_QUAD,\
+		Tween.EASE_OUT
+	)
+	return tween
 
 
-func _on_FirstRoomDetector_body_entered(body):
-	$Camera2D.current = true
-	$Camera2D2.current = false
+func _on_RoomDetector_1_body_entered(_body):
+	if currentRoom == 1:
+		currentRoom = 0
+		var tween = tweenIt(Vector2(976, 180), Vector2(320, 180))
+		tween.start()
+
+
+func _on_RoomDetector_2_body_entered(_body):
+	if currentRoom == 0:	
+		currentRoom = 1
+		var tween = tweenIt(Vector2(320, 180), Vector2(976, 180))
+		tween.start()
+		
+
+func _on_RoomDetector_3_body_entered(_body):
+	if currentRoom == 1:	
+		currentRoom = 2
+		var tween = tweenIt(Vector2(976, 180), Vector2(976, 544))
+		tween.start()
+
+func _on_RoomDetector_4_body_entered(_body):
+	if not activated_wall and currentRoom == 2:
+		$Camera2D.current = false
+		$Player/Camera2D.current = true
+		var invisWallInstance = invisWall.instance()
+		invisWallInstance.position = Vector2(667, 625)
+		self.add_child(invisWallInstance)
+		activated_wall = true
+
+func _on_Area2D_body_entered(body):
+	get_tree().paused = true
+	# Victory pop up here
